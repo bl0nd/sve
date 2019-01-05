9167db37066486a83e38fa8a23d30b2627ae7696
Jason Phan <xorblonded@gmail.com>
2019-01-04 21:48:33 -0600

    Added distro, config, and version gathering

--- sve/utils.py
+++ sve/utils.py
@@ -5,18 +5,98 @@ sve.utils
 
 This module has utility functions.
 """
+import re
 import sys
 import subprocess as sp
 
-def system_info():
-    system_os = sys.platform
-    if sys.platform.startswith("darwin"):
-        distro = sp.run(['head', '-n1', '/etc/os-release'])
-    elif sys.platform.startswith("darwin"):
-        # MacOS
-        pass
-    elif sys.platform("win32"):
-        #windows
-        pass
+def get_distro():
+    """Get name of OS/distribution.
 
-system_info()
+    If the OS is macOS or Windows, then the OS name is returned
+      instead of a distribution name.
+    """
+    system_os = sys.platform
+    if sys.platform.startswith("linux"):
+        with open('/etc/os-release', 'r') as f:
+            distro = f.readline().rstrip()[6:-1]
+    # elif sys.platform.startswith("darwin"):
+    # elif sys.platform.startswith("win32"):
+    return distro
+
+def get_configs(distro):
+    """Get locations of service config files.
+
+    :param distro: Name of Linux distribution.
+    :return configs: Dictionary of services and their config files.
+    :rtype: dict
+    """
+    if distro == 'Arch Linux':
+        configs = {'ftp': '/etc/vsftpd.conf',
+                   'ssh': '/etc/ssh/sshd_config',
+                   'apache': '/etc/https/conf/httpd.conf',
+                   'nginx': '/etc/nginx/nginx.conf',
+                   }
+    # elif distro == 'darwin':
+    # elif distro == 'win32':
+    else:
+        sys.exit('error: unknown Linux distribution')
+
+    return configs
+
+def get_ftp_version(distro):
+    """Get version number of FTP.
+
+    :param distro: Name of Linux distribution.
+    :return ftp_ver: Version number of FTp.
+    :rtype: str
+    """
+    if distro == 'Arch Linux':
+        ftp_ver_cmd = sp.run(['pacman', '-Q', 'vsftpd'], capture_output=True)
+        ftp_ver = ftp_ver_cmd.stdout.decode().rstrip().split(' ')[1]
+    # elif distro == 'darwin':
+    # elif distro == 'win32':
+    else:
+        sys.exit('error: unknown Linux distribution')
+
+    return ftp_ver
+
+def get_ssh_version(distro, ssh_config):
+    """Get version number of SSH.
+
+    :param distro: Name of Linux distribution.
+    :param ssh_config: Location of SSH configuration file.
+    :return ssh_ver: Version number of SSH.
+    :rtype: str
+
+    TODO:
+        1. May want to stay away from relying on config files.
+             They can be changed, depend on commands instead.
+    """
+    if distro == 'Arch Linux':
+        with open(ssh_config, 'r') as f:
+            ssh_ver_line = f.readline()
+        ssh_ver = ssh_ver_line.split('v ')[1].split(' ')[0]
+    # elif distro == 'darwin':
+    # elif distro == 'win32':
+    else:
+        sys.exit('error: unknown Linux distribution')
+
+    return ssh_ver
+
+def get_apache_version(distro):
+    """Get version number of Apache.
+
+    :param distro: Name of Linux distribution.
+    :return apache_ver: Version number of Apache.
+    :rtype: str
+    """
+    if distro == 'Arch Linux':
+        apache_ver_cmd = sp.run(['httpd', '-v'], capture_output=True)
+        apache_ver = re.search(r'\d*\.\d*\.\d*', apache_ver_cmd.stdout.decode()).group(0)
+    # elif distro == 'darwin':
+    # elif distro == 'win32':
+    else:
+        sys.exit('error: unknown Linux distribution')
+
+    return apache_ver
+
