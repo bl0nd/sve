c15ff5f40978867963087726dee8e3ef12c75956
Jason Phan <xormanatee@gmail.com>
2019-01-30 11:42:42 -0600

    removed: tests

--- tests/test_utils.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import subprocess as sp
-
-from sve.utils import (
-        get_distro, get_configs, get_ftp_version, get_ssh_version,
-        get_apache_version,
-        parse_services
-)
